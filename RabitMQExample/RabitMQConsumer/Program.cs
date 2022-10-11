e15b5b01c3ee77ce852fa0a072aec2cc5a087582
SabirQ <sabirmg@code.edu.az>
2022-10-11 09:50:42 +0400

    Class check

---
+++ RabitMQExample/RabitMQConsumer/Program.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+
+const string queue_name = "code-queue";
+var factor = new ConnectionFactory
+{
+    HostName = "localhost",
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection=factor.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(queue_name,durable:true,exclusive:false,autoDelete:false,arguments:null);
+
+
